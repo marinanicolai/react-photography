9e5f995d277598e68b77508d96d2f78676755611
marina <nicolaimarina@gmail.com>
2022-08-22 11:04:43 -0400

    adding the useState hook

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Contact() {
+  const [state, setState] = useState({});
+
+  const updateData = (e) => {
+    state[e.target.id] = e.target.value;
+  };
+
+  const getData = () => {
+    console.log(state);
+  };
+
   return (
     <div>
       <p>this is the contact page</p>
+      <input onChange={updateData} id="Name" />
+      <input onChange={updateData} id="lastName" />
+      <button onClick={getData}>Get Data</button>
+
       <Link to={"/"}>Home</Link>
     </div>
   );
